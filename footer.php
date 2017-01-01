0c8b3f7df7550aab0a448eb041635147f7dad751
Tim MacDonald <tim@Tims-MacBook-Pro-Retina.local>
2017-01-01 18:59:07 +1100

    version bump

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
         <?php if ('local' === ENV) : ?>
             <script src="//timacdonald.dev/wp-content/themes/theme/assets/theme.js"></script>
         <?php else : ?>
-            <script src="//cdn.timacdonald.me/website/theme.js?v=1"></script>
+            <script src="//cdn.timacdonald.me/website/theme.js?v=2"></script>
         <?php endif ?>
         <script>
         	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
--- header.php
+++ header.php
@@ -13,7 +13,7 @@
         <?php if ('local' === ENV) : ?>
             <link rel="stylesheet" href="//timacdonald.dev/wp-content/themes/theme/assets/theme.css">
         <?php else : ?>
-            <link rel="stylesheet" href="//cdn.timacdonald.me/website/theme.css?v=4">
+            <link rel="stylesheet" href="//cdn.timacdonald.me/website/theme.css?v=5">
         <?php endif ?>
     </head>
     <body>

4c0d44a757da9de126e8138dc0b67434cd042cf8
Tim MacDonald <tim@Tims-MacBook-Pro-Retina.local>
2017-01-03 00:25:53 +1100

    Style fix

--- templates/profile.php
+++ templates/profile.php
@@ -3,7 +3,7 @@
         <a class="dont-print-url" href="<?php bloginfo('url') ?>">
             <img src="<?php echo get_theme_mod('profile_image') ?>" alt="profile image" draggable="false" height="100" width="100">
         </a>
-        <a class="dont-print-url" href="<?php bloginfo('url') ?>">
+        <a class="name dont-print-url" href="<?php bloginfo('url') ?>">
             <?php bloginfo('name') ?>
         </a>
     </p>

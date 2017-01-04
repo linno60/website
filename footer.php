bd6a2c10cfc04d85c43a3bc7025ff9db1dd72003
Tim MacDonald <tim@Tims-MacBook-Pro-Retina.local>
2017-01-04 18:17:52 +1100

    Improved tracking when editing. Few style tweak.

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
         </div>
         <?php wp_footer() ?>
-        <script async src="<?php echo get_asset_url('theme', 12, 'js') ?>"></script>
-        <?php if (is_local_env()) : ?>
+        <?php if (is_admin() || is_local_env()) : ?>
+            <script>window.dontTrack = true</script>
             <script async src="<?php echo get_asset_url('local.js') ?>"></script>
         <?php endif ?>
+        <script async src="<?php echo get_asset_url('theme', 12, 'js') ?>"></script>
     </body>
 </html>

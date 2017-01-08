be0a98e36bfae3605c13dd24e7ad74f713414a82
Tim MacDonald <tim.mac7@me.com>
2017-01-08 11:37:59 +1100

    Moved build files to dedicated folder

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const elixir = require('laravel-elixir');
-
-elixir((mix) => {
-    mix.sass('./buildable/theme.scss', './assets/theme.css')
-    .sass('./buildable/amp.scss', './assets/theme-amp.css')
-    .webpack('./buildable/theme.js', './assets/theme.js');
-});
